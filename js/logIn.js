2d9e0a27e5df54d73b396bdbe889e4d6750b84e4
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-07-25 18:23:19 +0200

    add required text login

--- js/logIn.js
+++ js/logIn.js
@@ -1,9 +1,28 @@
-function openSummary() {
-  window.location.href = "summary.html";
-}
 function openSignUp() {
   window.location.href = "signup.html";
 }
-function openIndex(){
+function openIndex() {
   window.location.href = "index.html";
+}
+
+/**
+ * 
+ * @returns 
+ */
+function requiredInput() {
+  let emailInput = document.getElementById("emailLogIn");
+  let passwordInput = document.getElementById("passwordLogIn");
+  let requiredEmail = document.getElementById("requiredEmail");
+  let requiredPassword = document.getElementById("requiredPassword");
+  let emailValue = emailInput.value;
+  let passwordValue = passwordInput.value;
+  requiredEmail.innerHTML = emailValue === "" ? "This field is required" : !emailValue.includes("@") ? `'${emailValue}' is not valid. Please use an @-sign` : "";
+  requiredPassword.innerHTML = passwordValue === "" ? "This field is required" : "";
+  if (requiredEmail.innerHTML || requiredPassword.innerHTML) return;
+  checkUserInput()
+}
+
+async function checkUserInput(){
+/* hier wird die Datenbank auf den nutzer durchsucht */
+window.location.href = "summary.html"
 }
