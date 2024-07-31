de0ad1459fc71f52431c3d75f4b28e580222c3a9
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-07-31 10:44:41 +0200

    change link log in url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://join-bc563-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://login-de5c5-default-rtdb.europe-west1.firebasedatabase.app/";
 const CONTACT_URL = "https://contacts-e1e72-default-rtdb.europe-west1.firebasedatabase.app/";
 const BOARD_URL = "https://board-c7512-default-rtdb.europe-west1.firebasedatabase.app/"
