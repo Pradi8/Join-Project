044119e3bc55bbad277f850c44950716a1ac09b4
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-08-05 17:11:04 +0200

    change user id

--- js/questTemplates.js
+++ js/questTemplates.js
@@ -1,36 +1,36 @@
 let guestTasks = {
   toDo: [
     {
-      title: ["Task 1"],
-      description: ["Beschreibung von Task 1"],
+      title: "Task 1",
+      description: "Beschreibung von Task 1",
       assignedTo: ["MA", "HL", "KP"],
-      dueDate: ["2024-08-01"],
+      dueDate: "2024-08-01",
       prio: {
         urgent: false,
         medium: true,
         low: false
       },
-      category: ["User Story"],
+      category: "User Story",
       subtasks: ["Subtask 1"]
     },
     {
-      title: ["Task 2"],
-      description: ["Beschreibung von Task 1"],
+      title: "Task 2",
+      description: "Beschreibung von Task 1",
       assignedTo: ["UA", "TS"],
-      dueDate: ["2024-08-31"],
+      dueDate: "2024-08-31",
       prio: {
         urgent: true,
         medium: false,
         low: false
       },
-      category: ["Technical Task"],
-      subtasks: ["Subtask 1"]
+      category: "Technical Task",
+      subtasks: ["Subtask 1", "Subtask 2"]
     }
   ],
   inProgress: [
     {
-      title: ["Progess"],
-      description: ["Beschreibung von Task 1"],
+      title: "Progess",
+      description: "Beschreibung von Task 1",
       assignedTo: ["OP"],
       dueDate: ["2024-10-11"],
       prio: {
@@ -38,37 +38,37 @@ let guestTasks = {
         medium: false,
         low: false
       },
-      category: ["Technical Task"],
-      subtasks: ["Subtask 1"]
+      category: "Technical Task",
+      subtasks: [""]
     }
   ],
   awaitFeedback: [
     {
-      title: ["Feedback"],
-      description: ["Beschreibung von Task 1"],
-      assignedTo: ["LA"],
-      dueDate: ["2025-01-01"],
+      title: "Feedback",
+      description: "Beschreibung von Task 1",
+      assignedTo:["LA"],
+      dueDate: "2025-01-01",
       prio: {
         urgent: true,
         medium: false,
         low: false
       },
-      category: ["User Story"],
+      category: "User Story",
       subtasks: [""]
     }
   ],
   done: [
     {
-      title: ["Done"],
-      description: ["Beschreibung von Task 1"],
+      title: "Done",
+      description: "Beschreibung von Task 1",
       assignedTo: ["User A"],
-      dueDate: [""],
+      dueDate:"",
       prio: {
         urgent: true,
         medium: false,
         low: false
       },
-      category: ["Kategorie A"],
+      category: "Kategorie A",
       subtasks: ["Subtask 1"]
     }
   ]
