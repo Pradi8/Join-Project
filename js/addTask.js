0004c74082862b02632a75db51ec56f7b8563b7c
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-08-05 20:25:55 +0200

    short code add task

--- js/addTask.js
+++ js/addTask.js
@@ -1,4 +1,4 @@
-let newTask = [];
+ let newTask = [];
 
 /**
  * get data from add_task
@@ -36,4 +36,45 @@ async function postData(path='', data={}) {
 function emptyFields() {
     document.getElementById('task-title').value = '';
     document.getElementById('task-description').value = '';
+} 
+
+// Vorschlag fürn kurzen Code.
+/* 
+let data = {
+  taskStatus: "",
+  title: "",
+  description: "",
+  assignedTo: {},
+  dueDate: "",
+  prio: {
+    urgent: false,
+    medium: false,
+    low: false,
+  },
+  category: "",
+  subtasks: {},
+};
+
+function createNewTask(task) {
+  data.title = document.getElementById("task-title").value;
+  data.description = document.getElementById("task-description").value;
+  data.assignedTo = { name1: "Name1", name2: "Name2" };
+  data.dueDate = "Datum";  
+  data.prio.urgent = true; // hier muss noch der button abgefragt werden ist nur ein beispiel zum testen gewesen
+  data.category = "testlauf";
+  data.subtasks = { subtask1: "testlauf1", subtask2: "mit subtask spielen" }; 
+  data.taskStatus = task;
+  postData(task);
 }
+
+async function postData() {
+  let response = await fetch(BOARD_URL + userId + ".json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return (responseToJson = await response.json());
+}
+ */
