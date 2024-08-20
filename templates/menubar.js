3ff91753e49fe6edc3133bc51df68c385a997c9b
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-08-20 06:46:37 +0200

    show onlay 2 letters in shortcut menu

--- templates/menubar.js
+++ templates/menubar.js
@@ -1,41 +1,41 @@
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        const file = element.getAttribute("w3-include-html");
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    const file = element.getAttribute("w3-include-html");
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
-    showInitials();
+  }
+  showInitials();
 }
 
 function showOverlayMenu() {
-    document.getElementById('menu-bar-avatar').classList.add('show-overlay-menu');
-    document.getElementById('menu-bar-avatar-mobile').classList.add('show-overlay-menu');
+  document.getElementById("menu-bar-avatar").classList.add("show-overlay-menu");
+  document.getElementById("menu-bar-avatar-mobile").classList.add("show-overlay-menu");
 }
 
-function logout(){
-    window.location.href = "index.html";
+function logout() {
+  window.location.href = "index.html";
 }
 
-function showInitials(){
-    loadUser();
-    document.getElementById("userShortcut").innerHTML = getInitials();
-    document.getElementById("userShortcutMobile").innerHTML = getInitials();
+function showInitials() {
+  loadUser();
+  document.getElementById("userShortcut").innerHTML = getInitials();
+  document.getElementById("userShortcutMobile").innerHTML = getInitials();
 }
 
 function getInitials() {
-    let words = userName.split(" ");
-    let initials = "";
-    for (let i = 0; i < words.length; i++) {
-      if (words[i].length > 0) {
-        initials += words[i].substring(0, 1).toUpperCase();
-      }
+  let words = userName.split(" ");
+  let initials = "";
+  if (words.length > 0) {
+    initials += words[0].charAt(0).toUpperCase();
+    if (words.length > 1) {
+      initials += words[words.length - 1].charAt(0).toUpperCase();
     }
-    return initials;
   }
-  
+  return initials;
+}
