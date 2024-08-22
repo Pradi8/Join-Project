30e747403da9d7f0d02aa5ddbe204acb319496ba
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-08-22 11:57:21 +0200

    drag and drop card

--- js/board.js
+++ js/board.js
@@ -1,15 +1,30 @@
 function createTask() {
-    return {
-      title: [],
-      description: [],
-      assignedTo: [],
-      dueDate: [],
-      prio: {
-        urgent: false,
-        medium: false,
-        low: false,
-      },
-      category: [],
-      subtasks: [],
-    };
-  }
+  return {
+    title: [],
+    description: [],
+    assignedTo: [],
+    dueDate: [],
+    prio: {
+      urgent: false,
+      medium: false,
+      low: false,
+    },
+    category: [],
+    subtasks: [],
+  };
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
