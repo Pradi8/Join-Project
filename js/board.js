587d8496243544435077c9738ce20a1a12ced375
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-08-22 13:03:06 +0200

    optimize drag and drop for mobile

--- js/board.js
+++ js/board.js
@@ -14,17 +14,29 @@ function createTask() {
   };
 }
 
-function allowDrop(ev) {
-  ev.preventDefault();
+
+// Funktion wird aufgerufen, wenn ein Drag-Vorgang beginnt
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+// Funktion erlaubt das Ablegen des Elements
+function allowDrop(event) {
+  event.preventDefault();
 }
 
-function drop(ev) {
-  ev.preventDefault();
-  let data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+// Funktion wird aufgerufen, wenn das Element abgelegt wird
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  var element = document.getElementById(data);
+  
+  // Finde das nächstgelegene übergeordnete Element mit der Klasse 'task'
+  var taskContainer = event.target.closest('.task');
+  
+  // Finde die Div mit der Klasse 'aktiv-tasks' innerhalb des taskContainer
+  var targetContainer = taskContainer.querySelector('.aktiv-tasks');
+  
+  // Füge das gezogene Element in die 'aktiv-tasks'-Div ein
+  targetContainer.appendChild(element);
 }
-
