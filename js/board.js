1491bad894036ed59334732eae57d58a02bd100c
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-08-22 15:11:17 +0200

    problem show empty task

--- js/board.js
+++ js/board.js
@@ -14,10 +14,10 @@ function createTask() {
   };
 }
 
-
 // Funktion wird aufgerufen, wenn ein Drag-Vorgang beginnt
 function drag(event) {
   event.dataTransfer.setData("text", event.target.id);
+  event.target.style.transform = "rotate(20deg)";
 }
 
 // Funktion erlaubt das Ablegen des Elements
@@ -26,11 +26,27 @@ function allowDrop(event) {
 }
 
 // Funktion wird aufgerufen, wenn das Element abgelegt wird
-function drop(event) {
+function drop(event, id) {
   event.preventDefault();
   let data = event.dataTransfer.getData("text");
   let element = document.getElementById(data);
-  let taskContainer = event.target.closest('.task');
-  let targetContainer = taskContainer.querySelector('.aktiv-tasks');
+  let taskContainer = event.target.closest(".task");
+  let targetContainer = taskContainer.querySelector(".aktiv-tasks");
+  let noTask = document.getElementById(id);
   targetContainer.appendChild(element);
+  element.style.transform = "rotate(0deg)";
+  if (targetContainer) {
+    noTask.classList.remove("no-tasks");
+  } 
+}
+
+function checkChange(id, task) {
+  let noTasks = document.getElementById(task);
+  console.log(document.getElementById(id).innerHTML);
+  if (id.innerHTML === "") {
+    noTasks.classList.add("no-tasks");
+  }
+}
+function showTest(){
+console.log(document.getElementById('cardsInProgress').innerHTML);
 }
